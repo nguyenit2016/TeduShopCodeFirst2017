fa2b7396f25fa06f962478ebb53b69a0d02c6924
nguyenit2016 <nguyenpv1302@gmail.com>
2017-08-28 21:31:14 +0700

    Register user

--- NUI.Web/Controllers/AccountController.cs
+++ NUI.Web/Controllers/AccountController.cs
@@ -1,6 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BotDetect.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using NUI.Common;
+using NUI.Model.Models;
+using NUI.Web.App_Start;
+using NUI.Web.Models;
+using System;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,12 +13,102 @@ namespace NUI.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public AccountController()
+        {
+        }
+
+        // GET: Account
         public ActionResult Login()
         {
             return View();
         }
+
+        [HttpGet]
         public ActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [CaptchaValidation("CaptchaCode", "registerCaptcha", "Mã xác nhận không đúng")]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userByEmail = await _userManager.FindByEmailAsync(model.Email);
+                if (userByEmail != null)
+                {
+                    ModelState.AddModelError("email", "Email đã tồn tại");
+                    return View(model);
+                }
+                var userByUserName = await _userManager.FindByNameAsync(model.UserName);
+                if (userByUserName != null)
+                {
+                    ModelState.AddModelError("email", "Tài khoản đã tồn tại");
+                    return View(model);
+                }
+                var user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    EmailConfirmed = true,
+                    BirthDay = DateTime.Now,
+                    FullName = model.FullName,
+                    PhoneNumber = model.Phone,
+                    Address = model.Address
+                };
+
+                await _userManager.CreateAsync(user, model.Password);
+
+                var adminUser = await _userManager.FindByEmailAsync(model.Email);
+                if (adminUser != null)
+                    await _userManager.AddToRolesAsync(adminUser.Id, new string[] { "User" });
+
+                if (!string.IsNullOrEmpty(model.Email))
+                {
+                    string content = System.IO.File.ReadAllText(Server.MapPath("/Assets/client/template/newuser.html"));
+                    content = content.Replace("{{UserName}}", adminUser.UserName);
+                    content = content.Replace("{{Link}}", ConfigHelper.GetByKey("CurrentLink") + "dang-nhap.html");
+
+                    MailHelper.SendMail(adminUser.Email, "Đăng ký thành công", content);
+                }
+
+                ViewData["SuccessMsg"] = "Đăng ký thành công";
+            }
+
             return View();
         }
     }
---
+++ NUI.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NUI.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Tên là bắt buộc")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Tên đăng nhập là bắt buộc")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc")]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Email là bắt buộc")]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không đúng")]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
